1b6ba351c00b76c74cd9ea41610e887e9898cb70
Deepak Jois <deepak.jois@gmail.com>
2025-03-31 10:55:27 +0530

    more fixes

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import eslintPluginAstro from 'eslint-plugin-astro'
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.astro'] },
   {
     extends: [
       js.configs.recommended,
