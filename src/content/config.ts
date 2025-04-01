f980eb22162d3a3a3014a679b962e568e342d923
Deepak Jois <deepak.jois@gmail.com>
2025-04-01 22:00:46 +0530

    iteration 3

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
 import { defineCollection, z } from 'astro:content'
-import type { CollectionEntry } from 'astro:content'
 
 const articles = defineCollection({
   type: 'content',
@@ -8,17 +7,6 @@ const articles = defineCollection({
     date: z.date(),
     draft: z.boolean(),
   }),
-  slugField: ({
-    defaultSlug,
-    data,
-  }: {
-    defaultSlug: string
-    data: CollectionEntry<'articles'>['data']
-  }) => {
-    const date = data.date
-    const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
-    return `${formattedDate}-${defaultSlug}.html`
-  },
 })
 
 export const collections = {
